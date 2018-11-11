a55d7e79ec5a5a4c15e1d95b73a07c91d9881a6c
Arthur Tew <arthur@youngtew.com>
2018-11-12 07:27:43 +1030

    Updated prepare step

--- prepare-database.sh
+++ prepare-database.sh
@@ -18,7 +18,7 @@ initdb --username gnaf --pwfile=<(echo gnaf) --auth=md5 -D "$PGDATA/gnafdatabase
 pg_ctl -D /var/lib/postgresql/gnafdatabase -w start
 PGPASSWORD=gnaf psql -U gnaf postgres -c "create database gnafdata"
 PGPASSWORD=gnaf psql -U gnaf gnafdata -f /data/AUG18_GNAF_PipeSeparatedValue_20180827115521/G-NAF/Extras/GNAF_TableCreation_Scripts/create_tables_ansi.sql
-PGPASSWORD=gnaf psql -U gnaf gnafdata -f /data/AUG18_GNAF_PipeSeparatedValue_20180827115521/G-NAF/Extras/GNAF_TableCreation_Scripts/add_fk_constraints.sql
 for i in /app/*; do export CLASSPATH=$CLASSPATH:$i; done
 java -Xmx4G  cesa.enterprise.address.loader.Main -u gnaf -p gnaf -f /data/ -j jdbc:postgresql://localhost/gnafdata
+PGPASSWORD=gnaf psql -U gnaf gnafdata -f /data/AUG18_GNAF_PipeSeparatedValue_20180827115521/G-NAF/Extras/GNAF_TableCreation_Scripts/add_fk_constraints.sql
 pg_ctl -D /var/lib/postgresql/gnafdatabase stop
